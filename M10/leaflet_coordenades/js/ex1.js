cb4655797921d94f848270c2a41ed8b3c0214762
fbgis <fbgis@yahoo.es>
2021-02-24 19:48:14 +0100

    Segon lliurament M10. Leaflet coordenades

--- M10/leaflet_coordenades/js/ex1.js
+++ M10/leaflet_coordenades/js/ex1.js
@@ -3,9 +3,9 @@
 //Inicialització del mapa amb les coodenades i el zoom
 var mymap = L.map('mapid').setView([41.387, 2.170], 16);
 
-L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+var OpenStreetMap_HOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
 }).addTo(mymap);
 
 //Marcador a Balmes 16
